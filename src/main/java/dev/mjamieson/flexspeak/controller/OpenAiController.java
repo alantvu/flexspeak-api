7a9034fbb73796c7315ebeb98b01fd5764e20377
michaeljamieson <michaeljamieson10@gmail.com>
2023-02-04 00:50:52 -0500

    perhaps attempt to hook to front end

--- src/main/java/dev/mjamieson/flexspeak/controller/OpenAiController.java
+++ src/main/java/dev/mjamieson/flexspeak/controller/OpenAiController.java
@@ -1,6 +1,7 @@
 package dev.mjamieson.flexspeak.controller;
 
 import com.theokanning.openai.OpenAiService;
+import com.theokanning.openai.completion.CompletionChoice;
 import com.theokanning.openai.completion.CompletionRequest;
 import dev.mjamieson.flexspeak.integration.external.spacex.service.external_api.SpaceXCapsuleService;
 import dev.mjamieson.flexspeak.service.OpenAIService;
@@ -9,6 +10,8 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/open_ai")
@@ -25,10 +28,10 @@ public class OpenAiController {
 //        return spaceXCapsuleService.getAllCapsules().toString();
         OpenAiService service = new OpenAiService(apiKey);
         CompletionRequest completionRequest = CompletionRequest.builder()
-                .prompt("I like cats echo me")
-                .echo(true)
+                .prompt("can you tell me about main coone cats")
+                .echo(false)
                 .build();
-        service.createCompletion("ada",completionRequest).getChoices().forEach(System.out::println);
+        List<CompletionChoice> completionChoices = service.createCompletion("ada",completionRequest).getChoices();
         return "Hello World";
     }
 
