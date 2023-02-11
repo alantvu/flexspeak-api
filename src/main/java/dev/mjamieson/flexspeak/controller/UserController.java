325b0ab72b7e0f423fc72ba94292a527ab987456
michaeljamieson <michaeljamieson10@gmail.com>
2023-02-11 09:32:17 -0500

    able to reach here from react native

---
+++ src/main/java/dev/mjamieson/flexspeak/controller/UserController.java
@@ -0,0 +1,17 @@
+package dev.mjamieson.flexspeak.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/user")
+@RequiredArgsConstructor
+public class UserController {
+    @GetMapping
+    public String index() {
+
+        return "Hello World";
+    }
+}
