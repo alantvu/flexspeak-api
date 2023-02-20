ce03306eee83298544c6dc45b32320c17415c36e
michaeljamieson <michaeljamieson10@gmail.com>
2023-02-19 22:36:20 -0500

    pom succeeded here

--- src/test/java/dev/mjamieson/flexspeak/FlexspeakApplicationTests.java
+++ src/test/java/dev/mjamieson/flexspeak/FlexspeakApplicationTests.java
@@ -1,23 +1,12 @@
 package dev.mjamieson.flexspeak;
 
-import dev.mjamieson.flexspeak.integration.external.spacex.service.external_api.SpaceXCapsuleService;
 import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class FlexspeakApplicationTests {
-
-	private final SpaceXCapsuleService spaceXCapsuleService;
-
-	@Autowired
-	FlexspeakApplicationTests(SpaceXCapsuleService spaceXCapsuleService) {
-		this.spaceXCapsuleService = spaceXCapsuleService;
-	}
-
 	@Test
 	void contextLoads() {
-		spaceXCapsuleService.getAllCapsules();
 
 	}
 
