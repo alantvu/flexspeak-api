8a4256fabd3a421ad9d1830d02f5f74e7ceec472
michaeljamieson <michaeljamieson10@gmail.com>
2023-03-09 11:41:02 -0500

    Testcontainers added here able to populate from flyway tables

--- src/test/java/dev/mjamieson/flexspeak/FlexspeakApplicationTests.java
+++ src/test/java/dev/mjamieson/flexspeak/FlexspeakApplicationTests.java
@@ -1,45 +1,39 @@
 package dev.mjamieson.flexspeak;
 
-import dev.mjamieson.flexspeak.feature.model.Sentence;
-import dev.mjamieson.flexspeak.feature.model.Word;
-import dev.mjamieson.flexspeak.feature.open_ai.OpenAI_Service;
-import dev.mjamieson.flexspeak.feature.user.auth.AuthenticationService;
-import dev.mjamieson.flexspeak.feature.user.auth.RegisterRequest;
+
+import org.flywaydb.core.Flyway;
 import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
 
-import java.time.LocalDateTime;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
+import org.testcontainers.containers.PostgreSQLContainer;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
 
-@SpringBootTest
-class FlexspeakApplicationTests {
-	private final OpenAI_Service openAI_service;
-	private final AuthenticationService authenticationService;
-	@Autowired
-	public FlexspeakApplicationTests(OpenAI_Service openAI_service, AuthenticationService authenticationService) {
-		this.openAI_service = openAI_service;
-		this.authenticationService = authenticationService;
-	}
-
-	@Test
-	void contextLoads() {
-		RegisterRequest registerRequest = RegisterRequest.builder()
-				.firstname("mike")
-				.lastname("jamieson")
-				.email("michaeljamieson1@gmail.com")
-				.password("password")
-				.build();
-//		authenticationService.register(registerRequest);
-		List<Word> wordList = new ArrayList<>();
-		wordList.addAll(Arrays.asList(new Word("I", LocalDateTime.now()), new Word("like", LocalDateTime.now())));
+import static org.assertj.core.api.Assertions.assertThat;
 
 
-		Sentence sentence = Sentence.builder().words(wordList).sentence("I like eat chicken").build();
-		openAI_service.post(registerRequest.getEmail(),sentence);
+@Testcontainers
+public class FlexspeakApplicationTests {
 
-	}
+    @Container
+    private static PostgreSQLContainer<?> postgreSQLContainer = new PostgreSQLContainer<>("postgres:latest")
+            .withDatabaseName("dao-unit-test")
+            .withUsername("admin")
+            .withPassword("password");
 
+    @Test
+    void canStartPostgresDB() {
+        assertThat(postgreSQLContainer.isRunning()).isTrue();
+        assertThat(postgreSQLContainer.isCreated()).isTrue();
+    }
+
+    @Test
+    void canMigrateWithFlyway() {
+        //Given
+        //When
+
+        //Then
+        Flyway flyway = Flyway.configure().dataSource(postgreSQLContainer.getJdbcUrl(), postgreSQLContainer.getUsername(), postgreSQLContainer.getPassword()).load();
+        flyway.migrate();
+        System.out.println();
+    }
 }
