d3abf64688d2a897006e9ad117b6d0fad804c49a
michaeljamieson <michaeljamieson10@gmail.com>
2023-03-13 18:50:10 -0400

    davinci works updated package

--- src/main/java/dev/mjamieson/flexspeak/feature/open_ai/OpenAI_ServiceImpl.java
+++ src/main/java/dev/mjamieson/flexspeak/feature/open_ai/OpenAI_ServiceImpl.java
@@ -51,10 +51,11 @@ public class OpenAI_ServiceImpl implements OpenAI_Service {
                 .bestOf(1)
                 .stop(stopList)
                 .echo(false)
+                .model("davinci")
                 .build();
 //        List<CompletionChoice> completionChoices = openAiService.createCompletion("ada:ft-personal:grammar-plus-2023-03-05-05-05-48",completionRequest).getChoices();
 //        List<CompletionChoice> completionChoices = openAiService.createCompletion("gpt-3.5-turbo",completionRequest).getChoices();
-        List<CompletionChoice> completionChoices = openAiService.createCompletion("davinci",completionRequest).getChoices();
+        List<CompletionChoice> completionChoices = openAiService.createCompletion(completionRequest).getChoices();
         String aiSentence = completionChoices.get(0).getText();
 
         return Sentence.builder()
