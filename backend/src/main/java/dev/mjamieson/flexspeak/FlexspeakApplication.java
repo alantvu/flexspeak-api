962c5ddee7589fb58f56ca0f512034c6d93911b2
michaeljamieson <michaeljamieson10@gmail.com>
2023-04-16 21:01:30 -0400

    pritning stuff lets see wtf is going on

--- backend/src/main/java/dev/mjamieson/flexspeak/FlexspeakApplication.java
+++ backend/src/main/java/dev/mjamieson/flexspeak/FlexspeakApplication.java
@@ -11,18 +11,6 @@ import java.time.Clock;
 public class FlexspeakApplication {
 
 	public static void main(String[] args) {
-		String dbHost = System.getProperty("db.host");
-		int dbPort = Integer.parseInt(System.getProperty("db.port"));
-		String dbUser = System.getProperty("db.user");
-		String dbPassword = System.getProperty("db.password");
-		String dbName = System.getProperty("db.name");
-
-		System.out.println("Database Host: " + dbHost);
-		System.out.println("Database Port: " + dbPort);
-		System.out.println("Database User: " + dbUser);
-		System.out.println("Database Password: " + dbPassword);
-		System.out.println("Database Name: " + dbName);
-
 		new SpringApplicationBuilder(FlexspeakApplication.class)
 				.build()
 				.run(args);
