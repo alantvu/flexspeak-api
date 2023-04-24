6b93b27b8036be8f910d033432b580f0056965b2
michaeljamieson <michaeljamieson10@gmail.com>
2023-04-24 09:45:48 -0400

    added created at for user

--- backend/src/main/java/dev/mjamieson/flexspeak/feature/user/User.java
+++ backend/src/main/java/dev/mjamieson/flexspeak/feature/user/User.java
@@ -2,10 +2,12 @@ package dev.mjamieson.flexspeak.feature.user;
 
 import jakarta.persistence.*;
 import lombok.*;
+import org.hibernate.annotations.CreationTimestamp;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 
+import java.time.LocalDateTime;
 import java.util.Collection;
 import java.util.List;
 
@@ -33,6 +35,9 @@ public class User implements UserDetails {
     }
 
     private String password;
+
+    @CreationTimestamp
+    private LocalDateTime createdAt;
 
     @Enumerated(EnumType.STRING)
     private Role role;
